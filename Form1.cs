3b6f2e44ca2f9a7047e94aa05f9662b367c5a6c4
Mishardo <david04veron@gmail.com>
2026-05-23 21:46:48 -0300

    punto e

--- Form1.cs
+++ Form1.cs
@@ -13,15 +13,14 @@ namespace Figuras
         public Form1()
         {
             InitializeComponent();
-            figuras = new Figura[3] 
-            {
-                new Circulo(60),
-                new Rectangulo(30,50),
-                new Cuadrado(45),
-            };
+figuras = new Figura[3] 
+{
+    new Circulo(30),
+    new Rectangulo(50,70),
+    new Cuadrado(90),
+};
 
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             Graphics gr = pictureBox1.CreateGraphics();
@@ -29,11 +28,11 @@ Random rnd = new Random();
 
 for (int i = 0; i < figuras.Length; i++)
 {
-    Color colorRandom = Color.FromArgb(
-        rnd.Next(256),
-        rnd.Next(256),
-        rnd.Next(256)
-    );
+Color colorRandom = Color.FromArgb(
+    rnd.Next(0, 200),
+    rnd.Next(0, 200),
+    rnd.Next(0, 200)
+);
 
     Pen pen = new Pen(colorRandom);
     figuras[i].Dibujar(pen, gr, i * 100, 50);
