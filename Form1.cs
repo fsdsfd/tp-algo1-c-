71bb577a54c10d0b1b683a222d59f4964e878cf5
Tomas A. Renggli <tomasrenggli@gmail.com>
2026-05-25 02:05:55 -0300

    Pequeña mejora en form1 y redaccion de readme

--- Form1.cs
+++ Form1.cs
@@ -13,33 +13,34 @@ namespace Figuras
         public Form1()
         {
             InitializeComponent();
-figuras = new Figura[5] 
-{
-    new Circulo(40),
-    new Cuadrado(60),
-    new TrianguloEquilatero(70),
-    new TrianguloIsosceles(80,60),
-    new Rectangulo(50,70)
 
-};
+            figuras = new Figura[5]
+            {
+                new Circulo(40),
+                new Cuadrado(60),
+                new TrianguloEquilatero(70),
+                new TrianguloIsosceles(80, 60),
+                new Rectangulo(50, 70)
+            };
 
         }
+
         private void button1_Click(object sender, EventArgs e)
         {
             Graphics gr = pictureBox1.CreateGraphics();
-Random rnd = new Random();
+            Random rnd = new Random();
 
-for (int i = 0; i < figuras.Length; i++)
-{
-Color colorRandom = Color.FromArgb(
-    rnd.Next(0, 200),
-    rnd.Next(0, 200),
-    rnd.Next(0, 200)
-);
+            for (int i = 0; i < figuras.Length; i++)
+            {
+                Color colorRandom = Color.FromArgb(
+                    rnd.Next(0, 200),
+                    rnd.Next(0, 200),
+                    rnd.Next(0, 200)
+                );
 
-    Pen pen = new Pen(colorRandom);
-    figuras[i].Dibujar(pen, gr, i * 80, 50);
-}
+                Pen pen = new Pen(colorRandom);
+                figuras[i].Dibujar(pen, gr, i * 80, 50);
+            }
 
         }
     }
